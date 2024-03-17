0d3e122c0bd7f5f2591072e2df13a623b5e4ada8
deepanshuhyb <deepanshuhyb@gmail.com>
2024-03-17 17:08:11 +0530

    routing

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-const lib =  ;
+const global = {
+    curretPage: window.location.pathname
+};
+
+function highlightActiveLink() {
+    const links = document.querySelectorAll('.nav-link');
+    links.forEach((link)=>{
+        if(link.getAttribute('href') === global.curretPage){
+            link.classList.add('active');
+        }
+    })
+};
+
+function init() {
+    highlightActiveLink();
+    switch (global.curretPage) {
+        case '/':
+        case '/index.html':
+            console.log('home')
+            break;
+        case '/shows.html':
+            console.log('shows');
+            break;
+        case '/movie-details.html':
+            console.log('movie-details');
+            break;
+        case '/tv-details.html':
+            console.log('tv-details');
+            break;
+        case '/search.html':
+            console.log('search');
+            break;
+        default:
+            break;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', init);
