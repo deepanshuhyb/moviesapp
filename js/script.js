be290b4804e48b3ee39959af45242fd7dda83471
deepanshuhyb <deepanshuhyb@gmail.com>
2024-03-19 00:53:32 +0530

    shows api

--- js/script.js
+++ js/script.js
@@ -33,6 +33,35 @@ async function displayPopularMovies() {
     })
 }
 
+async function displayPopularShows() {
+    const { results } = await fetchApi('tv/popular');
+    console.log(results);
+    results.forEach((show)=>{
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = 
+        `<a href="tv-details.html?id=${show.id}">
+        ${show.poster_path ? `<img
+        src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+        class="card-img-top"
+        alt="${show.name}"
+        />` : `<img
+        src="images/no-image.jpg"
+        class="card-img-top"
+        alt="${show.name}"
+        />`}
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${show.name}</h5>
+          <p class="card-text">
+            <small class="text-muted">Aired: ${show.first_air_date}</small>
+          </p>
+        </div>
+        `
+        document.querySelector('#popular-shows').appendChild(div);
+    })
+}
+
 async function fetchApi(endpoint) {
     const API_KEY = '6c1a2240bbeb7f139779642257b534e2';
 
@@ -66,6 +95,7 @@ function init() {
             break;
         case '/shows.html':
             console.log('shows');
+            displayPopularShows();
             break;
         case '/movie-details.html':
             console.log('movie-details');
